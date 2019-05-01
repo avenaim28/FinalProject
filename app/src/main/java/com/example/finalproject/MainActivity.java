e5838badab3cb0340940177fbc1c17779e112a6c
jacobavenaim <avenaim2@illinois.edu>
2019-04-30 21:38:21 -0500

    final

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,54 +1,88 @@
 package com.example.finalproject;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.graphics.PorterDuff;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.ProgressBar;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "ExchangeRate";
+
+    private RequestQueue requestQueue;
+
+    private String countryConversion;
+
+    private JSONObject exchangeRate;
+
+    private TextView input;
+
+    private TextView output;
+
+    private double currency;
+
+    private double conversion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner mySpinner1 = (Spinner) findViewById(R.id.spinner1);
+        Spinner mySpinner1 = findViewById(R.id.spinner1);
         ArrayAdapter<String> myAdapter1 = new ArrayAdapter<>(MainActivity.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
         myAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner1.setAdapter(myAdapter1);
 
-        Spinner mySpinner2 = (Spinner) findViewById(R.id.spinner2);
-        ArrayAdapter<String> myAdapter2 = new ArrayAdapter<>(MainActivity.this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
-        myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner2.setAdapter(myAdapter2);
+        requestQueue = Volley.newRequestQueue(this);
+
+        input = findViewById(R.id.Input);
+
+        output = findViewById(R.id.Output);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            countryConversion = mySpinner1.getSelectedItem().toString();
+            Log.d(TAG, "conversion");
+            startAPICall(countryConversion);
+            if (input == null) {
+                Log.d(TAG, "Try Again");
+            }
+            conversion = Double.parseDouble(input.getText().toString());
+        });
     }
+    void startAPICall(String value) {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://free.currencyconverterapi.com/api/v6/convert?q=USD_"+countryConversion+"&compact=ultra&apiKey=052dfd7a3ad1bb7867a9",
+                    null,
+                    response -> {
+                        Log.d(TAG, response.toString());
+                        try {
+                            //Log.d(TAG, exchangeRate.toString());
+                            currency = response.getDouble("USD_" + value);
+                            Log.d(TAG, String.valueOf(exchangeRate));
+                            output.setText(String.valueOf(currency * conversion));
+                        } catch (JSONException e) {
+                            Log.e(TAG, "Error");
+                        }
+                    }, error -> Log.w(TAG, error.toString()));
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
